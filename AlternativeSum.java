1f229ed48d9a780e1175507cc995bc600a9e4a72
Rushikesh Danve <121048223+Rushikeshdanve@users.noreply.github.com>
2024-12-23 21:36:42 +0530

    add Array-Solved-Questions
    Simple Questions

---
+++ AlternativeSum.java
@@ -0,0 +1,12 @@
+public class AlternativeSum {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50,60};
+        int sum=0;
+
+        for(int i=0;i<arr.length;i=i+2){
+
+            sum=sum+arr[i];
+        }
+        System.out.println(sum);
+    }
+}
