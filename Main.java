428f36797c693b4a9355ccc764af6cf00480ff33
Rushikesh Danve <121048223+Rushikeshdanve@users.noreply.github.com>
2024-12-23 22:09:10 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        // Declare and initialize the array with a size of 5
         int[] arr = new int[5];
 
-        // Populate the array
         for (int i = 0; i < 5; i++) {
-            arr[i] = i; // Assign values to the array
+            arr[i] = i; 
         }
 
-        // Print the elements of the array
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]); // Print each element
+            System.out.println(arr[i]);
         }
     }
 }
